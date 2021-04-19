93c11d156286dc1540588e1bd2bcdf8ce9ed560f
Sergei Kurnevich <sergei.kurnevich@broadcom.com>
2021-04-19 18:49:03 +0200

    Updated webpack to v5
    Signed-off-by: Sergei Kurnevich <sergei.kurnevich@broadcom.com>

--- webClient/webpack.config.js
+++ webClient/webpack.config.js
@@ -27,13 +27,77 @@ var config = {
     'filename': 'main.js',
   },
   'plugins': [
-    new CopyWebpackPlugin([
+    new CopyWebpackPlugin({ 
+      patterns: [
+        {
+          from: path.resolve(__dirname, './src/assets'),
+          to: path.resolve('../web/assets')
+        }
+      ]}
+    )
+  ],
+  'module': {
+    'rules': [
       {
-        from: path.resolve(__dirname, './src/assets'),
-        to: path.resolve('../web/assets')
-      }
-    ])
-  ]
+        /* Javascript source map loader */
+        'enforce': 'pre',
+        'test': /\.js$/,
+        'loader': 'source-map-loader',
+        'exclude': [
+          /\/node_modules\//
+        ]
+      },
+      {
+        /* JSON inline loader */
+        'test': /\.json$/,
+        'loader': 'json-loader'
+      },
+      {
+        /* HTML URL resolution loader */
+        'test': /\.html$/,
+        'loader': 'html-loader'
+      },
+      {
+        'test': /\.svg$/,
+        'loader': 'svg-sprite-loader'
+      },
+      {
+        /* External file loader */
+        'test': /\.eot$/,
+        'loader': 'file-loader',
+        'options': {
+          'name': '[name].[hash:20].[ext]'
+        }
+      },
+      {
+        /* External (or inline) file loader */
+        'test': /\.(jpg|png|gif|otf|ttf|woff|woff2|cur|ani)$/,
+        'loader': 'url-loader',
+        'options': {
+          'name':'[name].[hash:20].[ext]', 
+          'limit': '10000'
+        }
+      },
+      {
+        /* CSS URL loader, TODO: reconsider */
+        'test': /\.css$/,
+        'use': [
+          'exports-loader?module.exports.toString()',
+          {
+            'loader': 'css-loader',
+            'options': {
+              'sourceMap': false
+            }
+          }
+        ]
+      },
+      {
+        /* TS and angular loader */
+        'test': /\.(ts|tsx)$/,
+        'loader': 'ts-loader',
+      }      
+    ]
+  }
 };
 
 function deepMerge(base, extension) {
