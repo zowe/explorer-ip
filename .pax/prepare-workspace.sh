6c158ccd59ea8ab358cf0256f7a0a87d1d414644
Nakul Manchanda <nakul.manchanda@ibm.com>
2021-04-20 09:44:21 -0400

    specify node version to use in prepare-workspace.sh
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- .pax/prepare-workspace.sh
+++ .pax/prepare-workspace.sh
@@ -15,7 +15,7 @@
 # 
 # - build client
 #################################################################################
-
+nvm use 10.24.1
 # constants
 SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
