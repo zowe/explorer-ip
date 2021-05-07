eb604f9b3207d843d54010e4886c5b99ea68807e
Tom Zhang <tom.yczhang@gmail.com>
2021-05-07 10:56:42 -0400

    Refactor some unused variables
    Signed-off-by: Tom Zhang <tom.yczhang@gmail.com>

--- dataService/test/fvt-scripts/prepare-fvt.sh
+++ dataService/test/fvt-scripts/prepare-fvt.sh
@@ -1,33 +1,25 @@
 # constants
 USERNAME=$1
-SCRIPT_NAME=$(basename "$0")
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 ZSS_DIR=${SCRIPT_DIR}/zss
 TEST_DIR=$(cd "${SCRIPT_DIR}/dataService/test" && pwd)
-export ROOT_DIR=$SCRIPT_DIR
-export INSTANCE_DIR=${ROOT_DIR}/instance
+export INSTANCE_DIR=${SCRIPT_DIR}/instance
 
 # prepare instance directory
-mkdir -p "${ROOT_DIR}/instance/workspace/app-server/serverConfig"
-mkdir -p "${ROOT_DIR}/instance/workspace/app-server/plugins"
+mkdir -p "${INSTANCE_DIR}/workspace/app-server/serverConfig"
+mkdir -p "${INSTANCE_DIR}/workspace/app-server/plugins"
 cd $TEST_DIR
-cp fvt-scripts/dummy-server.json "${ROOT_DIR}/instance/workspace/app-server/serverConfig/server.json"
-cp fvt-scripts/org.zowe.explorer-ip.json "${ROOT_DIR}/instance/workspace/app-server/plugins/org.zowe.explorer-ip.json"
-
-#correct tagging
-#chtag -Rtc IBM-1047 $TEST_DIR/fvt-scripts
-#chtag -Rtc ISO8859-1 $ZSS_DIR
+cp fvt-scripts/dummy-server.json "${INSTANCE_DIR}/workspace/app-server/serverConfig/server.json"
+cp fvt-scripts/org.zowe.explorer-ip.json "${INSTANCE_DIR}/workspace/app-server/plugins/org.zowe.explorer-ip.json"
 
 #pre-cleanup
-cd $TEST_DIR/fvt-scripts
-./cleanup.sh $USERNAME
+cd $TEST_DIR/fvt-scripts && ./cleanup.sh $USERNAME
 
 #build zss
 cd $ZSS_DIR/build && ./build.sh 
 
 #start zis
-cd $TEST_DIR/fvt-scripts
-./zis-start.sh $USERNAME ${ZSS_DIR} ${TEST_DIR}/fvt-scripts
+cd $TEST_DIR/fvt-scripts && ./zis-start.sh $USERNAME ${ZSS_DIR} ${TEST_DIR}/fvt-scripts
 
 # start zss
 cd $ZSS_DIR/bin
