15c567a52a2b5d8e184bdc2d56a7cf11d9622e36
Nakul Manchanda <nakul.manchanda@ibm.com>
2021-06-24 14:48:37 -0400

    explorer-ip install update
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,2 @@
-cd lib && extattr +p ipExplorer.so
-cd ../web/assets && chtag -b *.png
+cd ${ROOT_DIR}/components/app-server/share/explorer-ip/lib && extattr +p ipExplorer.so
+cd ${ROOT_DIR}/components/app-server/share/explorer-ip/web/assets && chtag -b *.png
