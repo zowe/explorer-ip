a90a2109c3a588624222aca60af90944f90cbee9
1000TurquoisePogs <sgrady@rocketsoftware.com>
2022-08-04 16:09:39 -0400

    Add trivial schema, update zss builder, and manifest, and changelog
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# IP Explorer Changelog
+
+All notable changes to the sample angular app will be documented in this file.
+
+## 1.0.1
+
+- Bugfix: Schema file was not included, preventing installation as a component
+- Bugfix: Manifest build content template was never resolved, so it has been removed.
+
--- dataService/build/build31.sh
+++ dataService/build/build31.sh
@@ -22,7 +22,7 @@ rm -f "${TARGET}"
 if ! c89 -D_OPEN_THREADS -D_XOPEN_SOURCE=600 -DAPF_AUTHORIZED=0 -DNOIBMHTTP \
 "-Wa,goff" "-Wc,langlvl(EXTC99),float(HEX),agg,expo,list(),so(),search(),\
 goff,xref,gonum,roconst,gonum,asm,asmlib('SYS1.MACLIB'),asmlib('CEE.SCEEMAC'),dll" -Wl,dll \
--I ${ZSS}/h -I ${ZOWECOMMON}/h \
+-I ${ZSS}/h -I ${ZOWECOMMON}/h -I ${ZOWECOMMON}/platform/posix \
 -o "${TARGET}" \
 ../../src/ipExplorerDataService.c \
 ../pluginAPI31.x 
